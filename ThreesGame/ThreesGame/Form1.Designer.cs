44611b1df925df34016433fac83ac2f291f0790a
Tâm Anh Lê <tamanhlethi1@gmail.com>
2017-11-27 14:04:12 +0700

    game threes

---
+++ ThreesGame/ThreesGame/Form1.Designer.cs
@@ -0,0 +1,347 @@
+﻿namespace ThreesGame
+{
+    partial class frmThreesGame
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.lbl3 = new System.Windows.Forms.Label();
+            this.lbl4 = new System.Windows.Forms.Label();
+            this.lbl5 = new System.Windows.Forms.Label();
+            this.lbl6 = new System.Windows.Forms.Label();
+            this.lbl7 = new System.Windows.Forms.Label();
+            this.lbl8 = new System.Windows.Forms.Label();
+            this.lbl9 = new System.Windows.Forms.Label();
+            this.lbl13 = new System.Windows.Forms.Label();
+            this.lbl10 = new System.Windows.Forms.Label();
+            this.lbl14 = new System.Windows.Forms.Label();
+            this.lbl11 = new System.Windows.Forms.Label();
+            this.lbl15 = new System.Windows.Forms.Label();
+            this.lbl12 = new System.Windows.Forms.Label();
+            this.lbl16 = new System.Windows.Forms.Label();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.continueToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lbl1
+            // 
+            this.lbl1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lbl1.BackColor = System.Drawing.Color.White;
+            this.lbl1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lbl1.Location = new System.Drawing.Point(8, 83);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(50, 50);
+            this.lbl1.TabIndex = 0;
+            this.lbl1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl2
+            // 
+            this.lbl2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lbl2.BackColor = System.Drawing.Color.White;
+            this.lbl2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lbl2.Location = new System.Drawing.Point(61, 83);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(50, 50);
+            this.lbl2.TabIndex = 0;
+            this.lbl2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl3
+            // 
+            this.lbl3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lbl3.BackColor = System.Drawing.Color.White;
+            this.lbl3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lbl3.Location = new System.Drawing.Point(113, 83);
+            this.lbl3.Name = "lbl3";
+            this.lbl3.Size = new System.Drawing.Size(50, 50);
+            this.lbl3.TabIndex = 0;
+            this.lbl3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl4
+            // 
+            this.lbl4.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lbl4.BackColor = System.Drawing.Color.White;
+            this.lbl4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lbl4.Location = new System.Drawing.Point(165, 83);
+            this.lbl4.Name = "lbl4";
+            this.lbl4.Size = new System.Drawing.Size(50, 50);
+            this.lbl4.TabIndex = 0;
+            this.lbl4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl5
+            // 
+            this.lbl5.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lbl5.BackColor = System.Drawing.Color.White;
+            this.lbl5.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lbl5.Location = new System.Drawing.Point(8, 136);
+            this.lbl5.Name = "lbl5";
+            this.lbl5.Size = new System.Drawing.Size(50, 50);
+            this.lbl5.TabIndex = 0;
+            this.lbl5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl6
+            // 
+            this.lbl6.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lbl6.BackColor = System.Drawing.Color.White;
+            this.lbl6.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lbl6.Location = new System.Drawing.Point(61, 136);
+            this.lbl6.Name = "lbl6";
+            this.lbl6.Size = new System.Drawing.Size(50, 50);
+            this.lbl6.TabIndex = 0;
+            this.lbl6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl7
+            // 
+            this.lbl7.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lbl7.BackColor = System.Drawing.Color.White;
+            this.lbl7.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lbl7.Location = new System.Drawing.Point(113, 136);
+            this.lbl7.Name = "lbl7";
+            this.lbl7.Size = new System.Drawing.Size(50, 50);
+            this.lbl7.TabIndex = 0;
+            this.lbl7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl8
+            // 
+            this.lbl8.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lbl8.BackColor = System.Drawing.Color.White;
+            this.lbl8.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lbl8.Location = new System.Drawing.Point(165, 136);
+            this.lbl8.Name = "lbl8";
+            this.lbl8.Size = new System.Drawing.Size(50, 50);
+            this.lbl8.TabIndex = 0;
+            this.lbl8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl9
+            // 
+            this.lbl9.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lbl9.BackColor = System.Drawing.Color.White;
+            this.lbl9.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lbl9.Location = new System.Drawing.Point(8, 187);
+            this.lbl9.Name = "lbl9";
+            this.lbl9.Size = new System.Drawing.Size(50, 50);
+            this.lbl9.TabIndex = 0;
+            this.lbl9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl13
+            // 
+            this.lbl13.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lbl13.BackColor = System.Drawing.Color.White;
+            this.lbl13.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lbl13.Location = new System.Drawing.Point(8, 240);
+            this.lbl13.Name = "lbl13";
+            this.lbl13.Size = new System.Drawing.Size(50, 50);
+            this.lbl13.TabIndex = 0;
+            this.lbl13.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl10
+            // 
+            this.lbl10.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lbl10.BackColor = System.Drawing.Color.White;
+            this.lbl10.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lbl10.Location = new System.Drawing.Point(61, 187);
+            this.lbl10.Name = "lbl10";
+            this.lbl10.Size = new System.Drawing.Size(50, 50);
+            this.lbl10.TabIndex = 0;
+            this.lbl10.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl14
+            // 
+            this.lbl14.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lbl14.BackColor = System.Drawing.Color.White;
+            this.lbl14.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lbl14.Location = new System.Drawing.Point(61, 240);
+            this.lbl14.Name = "lbl14";
+            this.lbl14.Size = new System.Drawing.Size(50, 50);
+            this.lbl14.TabIndex = 0;
+            this.lbl14.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl11
+            // 
+            this.lbl11.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lbl11.BackColor = System.Drawing.Color.White;
+            this.lbl11.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lbl11.Location = new System.Drawing.Point(113, 187);
+            this.lbl11.Name = "lbl11";
+            this.lbl11.Size = new System.Drawing.Size(50, 50);
+            this.lbl11.TabIndex = 0;
+            this.lbl11.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl15
+            // 
+            this.lbl15.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lbl15.BackColor = System.Drawing.Color.White;
+            this.lbl15.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lbl15.Location = new System.Drawing.Point(113, 240);
+            this.lbl15.Name = "lbl15";
+            this.lbl15.Size = new System.Drawing.Size(50, 50);
+            this.lbl15.TabIndex = 0;
+            this.lbl15.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl12
+            // 
+            this.lbl12.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lbl12.BackColor = System.Drawing.Color.White;
+            this.lbl12.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lbl12.Location = new System.Drawing.Point(165, 187);
+            this.lbl12.Name = "lbl12";
+            this.lbl12.Size = new System.Drawing.Size(50, 50);
+            this.lbl12.TabIndex = 0;
+            this.lbl12.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl16
+            // 
+            this.lbl16.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lbl16.BackColor = System.Drawing.Color.White;
+            this.lbl16.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lbl16.Location = new System.Drawing.Point(165, 240);
+            this.lbl16.Name = "lbl16";
+            this.lbl16.Size = new System.Drawing.Size(50, 50);
+            this.lbl16.TabIndex = 0;
+            this.lbl16.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblScore
+            // 
+            this.lblScore.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblScore.BackColor = System.Drawing.Color.DarkRed;
+            this.lblScore.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblScore.Location = new System.Drawing.Point(143, 34);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(72, 36);
+            this.lblScore.TabIndex = 1;
+            this.lblScore.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(228, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuToolStripMenuItem
+            // 
+            this.menuToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newGameToolStripMenuItem,
+            this.continueToolStripMenuItem});
+            this.menuToolStripMenuItem.Name = "menuToolStripMenuItem";
+            this.menuToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.menuToolStripMenuItem.Text = "M&enu";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.exitToolStripMenuItem.Text = "&Exit";
+            // 
+            // newGameToolStripMenuItem
+            // 
+            this.newGameToolStripMenuItem.Name = "newGameToolStripMenuItem";
+            this.newGameToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.newGameToolStripMenuItem.Text = "New Game";
+            // 
+            // continueToolStripMenuItem
+            // 
+            this.continueToolStripMenuItem.Name = "continueToolStripMenuItem";
+            this.continueToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.continueToolStripMenuItem.Text = "Continue";
+            // 
+            // frmThreesGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gainsboro;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(228, 303);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.lbl16);
+            this.Controls.Add(this.lbl8);
+            this.Controls.Add(this.lbl12);
+            this.Controls.Add(this.lbl4);
+            this.Controls.Add(this.lbl15);
+            this.Controls.Add(this.lbl7);
+            this.Controls.Add(this.lbl11);
+            this.Controls.Add(this.lbl3);
+            this.Controls.Add(this.lbl14);
+            this.Controls.Add(this.lbl6);
+            this.Controls.Add(this.lbl10);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.lbl13);
+            this.Controls.Add(this.lbl9);
+            this.Controls.Add(this.lbl5);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmThreesGame";
+            this.Text = "ThreesGame";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.Label lbl3;
+        private System.Windows.Forms.Label lbl4;
+        private System.Windows.Forms.Label lbl5;
+        private System.Windows.Forms.Label lbl6;
+        private System.Windows.Forms.Label lbl7;
+        private System.Windows.Forms.Label lbl8;
+        private System.Windows.Forms.Label lbl9;
+        private System.Windows.Forms.Label lbl13;
+        private System.Windows.Forms.Label lbl10;
+        private System.Windows.Forms.Label lbl14;
+        private System.Windows.Forms.Label lbl11;
+        private System.Windows.Forms.Label lbl15;
+        private System.Windows.Forms.Label lbl12;
+        private System.Windows.Forms.Label lbl16;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menuToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newGameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem continueToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
+
---
+++ ThreesGame/ThreesGame/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ThreesGame
+{
+    public partial class frmThreesGame : Form
+    {
+        Random Rd = new Random();
+        int[,] mangChiSoRong = new int[4, 4];
+        public frmThreesGame()
+        {
+            InitializeComponent();
+        }
+        public void CapNhatMau()
+        {
+            Label[,] Game = {
+                                {lbl1,lbl2,lbl3,lbl4},
+                                {lbl5,lbl6,lbl7,lbl8},
+                                {lbl9,lbl10,lbl11,lbl12},
+                                {lbl13,lbl14,lbl15,lbl16}
+                              };
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+
+                    if (Game[i, j].Text == "")
+                    {
+                        Game[i, j].BackColor = System.Drawing.Color.CadetBlue;
+                    }
+                    if (Game[i, j].Text == "1")
+                    {
+                        Game[i, j].BackColor = System.Drawing.Color.SkyBlue;
+                        Game[i, j].ForeColor = System.Drawing.Color.White;
+
+                    }
+                    if (Game[i, j].Text == "2")
+                    {
+                        Game[i, j].BackColor = System.Drawing.Color.Pink;
+                        Game[i, j].ForeColor = System.Drawing.Color.White;
+
+                    }
+                    if (Game[i, j].Text == "3")
+                    {
+                        Game[i, j].BackColor = System.Drawing.Color.White;
+                        Game[i, j].ForeColor = System.Drawing.Color.Black;
+
+                    }
+                    if (Game[i, j].Text == "6")
+                    {
+                        Game[i, j].BackColor = System.Drawing.Color.White;
+                        Game[i, j].ForeColor = System.Drawing.Color.Black;
+                    }
+                    if (Game[i, j].Text == "12")
+                    {
+                        Game[i, j].BackColor = System.Drawing.Color.White;
+                        Game[i, j].ForeColor = System.Drawing.Color.Black;
+                    }
+                    if (Game[i, j].Text == "24")
+                    {
+                        Game[i, j].BackColor = System.Drawing.Color.White;
+                        Game[i, j].ForeColor = System.Drawing.Color.Black;
+                    }
+                    if (Game[i, j].Text == "48")
+                    {
+                        Game[i, j].BackColor = System.Drawing.Color.White;
+                        Game[i, j].ForeColor = System.Drawing.Color.Black;
+                    }
+                    if (Game[i, j].Text == "96")
+                    {
+                        Game[i, j].BackColor = System.Drawing.Color.White;
+                        Game[i, j].ForeColor = System.Drawing.Color.Pink;
+                    }
+                    if (Game[i, j].Text == "192")
+                    {
+                        Game[i, j].BackColor = System.Drawing.Color.White;
+                        Game[i, j].ForeColor = System.Drawing.Color.Pink;
+                    }
+                    if (Game[i, j].Text == "384")
+                    {
+                        Game[i, j].BackColor = System.Drawing.Color.White;
+                        Game[i, j].ForeColor = System.Drawing.Color.Yellow;
+                    }
+                    if (Game[i, j].Text == "768")
+                    {
+                        Game[i, j].BackColor = System.Drawing.Color.White;
+                        Game[i, j].ForeColor = System.Drawing.Color.Yellow;
+                    }
+                    if (Game[i, j].Text == "1536")
+                    {
+                        Game[i, j].BackColor = System.Drawing.Color.White;
+                        Game[i, j].ForeColor = System.Drawing.Color.Gold;
+                    }
+                    if (Game[i, j].Text == "3042")
+                    {
+                        Game[i, j].BackColor = System.Drawing.Color.White;
+                        Game[i, j].ForeColor = System.Drawing.Color.Gold;
+                    }
+                    
+                }
+            }
+
+        }
+
+    }
+}
